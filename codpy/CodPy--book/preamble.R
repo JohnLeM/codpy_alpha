a58e0554d5065e53d1c9bfbc228b74e3e4c14f32
JohnLeM <jeanmarc.mercier@gmail.com>
2022-01-24 19:51:22 +0100

    forgot preambles files

---
+++ codpy/CodPy--book/preamble.R
@@ -0,0 +1,6 @@
+library(reticulate)
+library(knitr)
+library(kableExtra)
+require(rhandsontable)
+knitr::opts_chunk$set(echo = FALSE, fig.path='./CodPyTempFigs/', include = TRUE, results = "show",warning = FALSE, message = FALSE)
+########################################
---
+++ codpy/CodPy--book/preamble.py
@@ -0,0 +1,17 @@
+import os, sys
+import time 
+import numpy as np
+from pathlib import Path
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+if parentdir not in sys.path:sys.path.append(parentdir)
+from common_include import * 
+from data_generators import * 
+from predictors import * 
+from scikit_tools import * 
+from mnist_codpy import * 
+from housing_prices import * 
+
+plt.rc('font', size=6)
+print("preamble ok") 
+########################################
